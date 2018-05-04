c508605d456320065313776d04bdd5954bed8418
Mehdi <medev.kym@gmail.com>
2018-05-04 16:45:50 +0200

    Add Calculation.swift

---
+++ Zozor+/Model/Calculation.swift
@@ -0,0 +1,13 @@
+//
+//  Calculation.swift
+//  CountOnMe
+//
+//  Created by Mehdi on 04/05/2018.
+//  Copyright © 2018 Ambroise Collon. All rights reserved.
+//
+
+import Foundation
+
+class Calculation {
+    
+}
