9b59ebecbb6e6a9f8af0da4454d2a1d025d81281
Mehdi <medev.kym@gmail.com>
2018-05-12 17:42:14 +0200

    Add AlertMessage.swift

---
+++ Zozor+/AlertMessage.swift
@@ -0,0 +1,13 @@
+//
+//  AlertMessage.swift
+//  CountOnMe
+//
+//  Created by Mehdi on 12/05/2018.
+//  Copyright © 2018 Ambroise Collon. All rights reserved.
+//
+
+import Foundation
+
+protocol AlertMessage {
+    
+}
